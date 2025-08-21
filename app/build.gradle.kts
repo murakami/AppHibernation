f914f5e3b772efb43243888646b02d880270c5ef
Yukio Murakami <m_yukio@mac.com>
2025-08-21 14:47:29 +0900

    休止状態

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -45,4 +45,34 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+
+    implementation("androidx.concurrent:concurrent-futures:1.3.0")
+    implementation("androidx.concurrent:concurrent-futures-ktx:1.3.0")
+
+    implementation("com.google.guava:guava:31.0.1-android")
+    // To use CallbackToFutureAdapter
+    //implementation("androidx.concurrent:concurrent-futures:1.3.0")
+    // Kotlin
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-guava:1.6.0")
+
+    val core_version = "1.17.0"
+    // Java language implementation
+    implementation("androidx.core:core:$core_version")
+    // Kotlin
+    implementation("androidx.core:core-ktx:$core_version")
+    // To use RoleManagerCompat
+    implementation("androidx.core:core-role:1.1.0")
+    // To use the Animator APIs
+    implementation("androidx.core:core-animation:1.0.0")
+    // To test the Animator APIs
+    androidTestImplementation("androidx.core:core-animation-testing:1.0.0")
+    // Optional - To enable APIs that query the performance characteristics of GMS devices.
+    implementation("androidx.core:core-performance:1.0.0")
+    // Optional - to use ShortcutManagerCompat to donate shortcuts to be used by Google
+    implementation("androidx.core:core-google-shortcuts:1.1.0")
+    // Optional - to support backwards compatibility of RemoteViews
+    implementation("androidx.core:core-remoteviews:1.1.0")
+    // Optional - APIs for SplashScreen, including compatibility helpers on devices prior Android 12
+    implementation("androidx.core:core-splashscreen:1.2.0-rc01")
+
 }
--- app/src/main/java/com/example/apphibernation/MainActivity.kt
+++ app/src/main/java/com/example/apphibernation/MainActivity.kt
@@ -1,10 +1,17 @@
 package com.example.apphibernation
 
+import android.content.Context
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
+import androidx.core.content.IntentCompat
+import androidx.core.content.PackageManagerCompat
+import androidx.core.content.UnusedAppRestrictionsConstants
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.google.common.util.concurrent.ListenableFuture
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -16,5 +23,52 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+        appHibernation()
+    }
+
+    lateinit var future: ListenableFuture<Int>
+
+    fun appHibernation() {
+        Log.d("Hibernation", "appHibernation")
+        future = PackageManagerCompat.getUnusedAppRestrictionsStatus(this)
+        future.addListener({ onResult(future.get()) }, ContextCompat.getMainExecutor(this))
+    }
+
+    private val REQUEST_CODE: Int = 999
+
+    //val future: ListenableFuture<Int> =
+    //    PackageManagerCompat.getUnusedAppRestrictionsStatus(this)
+    //future.addListener({ onResult(future.get()) }, ContextCompat.getMainExecutor(context))
+
+    fun onResult(appRestrictionsStatus: Int) {
+        when (appRestrictionsStatus) {
+            // Couldn't fetch status. Check logs for details.
+            UnusedAppRestrictionsConstants.ERROR -> { Log.d("Hibernation", "ERROR") }
+
+            // Restrictions don't apply to your app on this device.
+            UnusedAppRestrictionsConstants.FEATURE_NOT_AVAILABLE -> { Log.d("Hibernation", "FEATURE_NOT_AVAILABLE") }
+
+            // The user has disabled restrictions for your app.
+            UnusedAppRestrictionsConstants.DISABLED -> { Log.d("Hibernation", "DISABLED") }
+
+            // If the user doesn't start your app for a few months, the system will
+            // place restrictions on it. See the API_* constants for details.
+            UnusedAppRestrictionsConstants.API_30_BACKPORT, UnusedAppRestrictionsConstants.API_30, UnusedAppRestrictionsConstants.API_31 -> handleRestrictions(appRestrictionsStatus)
+        }
+    }
+
+    fun handleRestrictions(appRestrictionsStatus: Int) {
+        Log.d("Hibernation", "handleRestrictions")
+        // If your app works primarily in the background, you can ask the user
+        // to disable these restrictions. Check if you have already asked the
+        // user to disable these restrictions. If not, you can show a message to
+        // the user explaining why permission auto-reset or app hibernation should be
+        // disabled. Then, redirect the user to the page in system settings where they
+        // can disable the feature.
+        val intent = IntentCompat.createManageUnusedAppRestrictionsIntent(this, packageName)
+
+        // You must use startActivityForResult(), not startActivity(), even if
+        // you don't use the result code returned in onActivityResult().
+        startActivityForResult(intent, REQUEST_CODE)
     }
 }
